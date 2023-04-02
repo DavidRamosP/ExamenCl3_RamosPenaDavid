3bf4c2ca0c404255e334e5e29c3341edf33446e1
USUARIO <david.emanuel.rp94@gmail.com>
2023-04-02 12:31:30 -0500

    modificacion de app.properties para conexion con clever cloud y restMapping

--- pe.com.cibertec_ExamenCL3/src/main/java/pe/com/cibertec/web/ControladorInicio.java
+++ pe.com.cibertec_ExamenCL3/src/main/java/pe/com/cibertec/web/ControladorInicio.java
@@ -4,14 +4,19 @@
  */
 package pe.com.cibertec.web;
 
+import java.util.List;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pe.com.cibertec.domain.Customer;
 import pe.com.cibertec.servicio.CustomerService;
 
@@ -19,60 +24,42 @@ import pe.com.cibertec.servicio.CustomerService;
  *
  * @author USUARIO
  */
-@Controller
-@Slf4j
+@RestController
+@RequestMapping("/api")
 public class ControladorInicio {
 
 @Autowired
 private CustomerService customerService; 
 
     
-    @GetMapping("/")
-    public String inicio(Model model){
-      
-        var customers = customerService.listarCustomers();
-        
-        model.addAttribute("customers", customers);
-        
-        
-        log.info("ejecutando controlador spring con MVC ");
-     
-    return "index";
+    @GetMapping("/custom")
+    public List<Customer> listarCustomers(){    
+        var customers = customerService.listarCustomers();  
+        return customers;
     }
     
-    @GetMapping("/agregar")
-    public String agregar(Customer customer){
-        
-        
-        return "modificar";
+    @PostMapping("/custom")
+    public void agregarCustomer( @RequestBody Customer customer){
+                customerService.guardar(customer);  
     }
     
-    @PostMapping("/guardar")
-    public String guardar(@Valid Customer customer, Errors errores){
-        if(errores.hasErrors()){
-        return "modificar";
-        }
-        
-        customerService.guardar(customer);
-        return "redirect:/";
-     }
 
-    @GetMapping("/editar/{id_customer}")
-    public String editar(Customer customer, Model model){
-        customer = customerService.encontrarCustomer(customer);
+    @GetMapping("/custom/{id_customer}")
+    public Customer editarCustomer(@PathVariable("id_customer") Long id){
+       Customer customer = new Customer();
+       customer.setId_customer(id);
+               
+     return     customerService.encontrarCustomer(customer);
         
-        model.addAttribute("customer", customer);
-        
-        return "modificar";
     }
     
-    @GetMapping("/eliminar/{id_producto}")
-    public String eliminar(Customer customer, Model model){
-        customerService.eliminar(customer);
-        
-        return "redirect:/";
+    @DeleteMapping("/custom/{id_customer}")
+    public void eliminarCustomer(@PathVariable("id_customer") Long id){
+       Customer customer = new Customer();
+       customer.setId_customer(id);
+       customerService.eliminar(customer);
     }
+    
 
-    
     
 }
